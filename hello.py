dba709fb6d1b4f7f2eb610299aa90f49bd6cfe64
Vijayakumar-2311 <134689049+Vijayakumar-2311@users.noreply.github.com>
2025-06-08 13:11:35 +0530

    new changes added

--- hello.py
+++ hello.py
@@ -3,3 +3,19 @@ def print_hi(name):
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
+"""
+1. git add hello.py - this means already file is staged 
+
+Ans -- now how to unstage it --> git reset hello.py
+
+2. Staged file – How to do changes in the code, Even if a file is staged (git add done), 
+
+Ans -- You can still open and modify it. If you do:-
+-- You’ll see the same file listed twice:
+-- Once in staged
+-- Once in unstaged
+
+
+"""
